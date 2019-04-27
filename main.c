5d43dd99609af12dfb9482466864b85562236d9c
Brendan Daley <brendandaley11@gmail.com>
2019-04-27 01:39:26 +0000

    Testing some stuff for the project

--- main.c
+++ main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 int main()
 {
-  return 0;
+    char str[10];
+    char newStr[10];
+    char c;
+    printf("Enter a string: \n");
+    scanf("%s", str);
+    printf("Read: %s\n", str);
+    
+    for (int i = 0; i < 4; i++)
+    {
+        c = str[i] + 1;
+        newStr[i] = c;
+    }
+    
+    printf("New string: %s\n", newStr);
 }
